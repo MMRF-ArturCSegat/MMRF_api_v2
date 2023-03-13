fe9708b1f0886e141e70acb2b1eb99fc032511ff
Artur Camera Segat <80114342+ArturCSegat@users.noreply.github.com>
2023-03-13 11:04:10 -0300

    model1

--- database/dbfuncs.go
+++ database/dbfuncs.go
@@ -1,4 +1,4 @@
-package db
+package db2
 
 import (
 	"errors"
@@ -10,30 +10,16 @@ import (
 var err error
 var db *gorm.DB
 
+
 type Node struct {
-    ID      int       `json:"id" gorm:"primaryKey"`
-    Lat     float64   `json:"lat"`
-    Lng     float64   `json:"lng"`
-    Edges   []*Edge   `json:"edges" gorm:"foreignKey:EID"	`
+	ID		int		`json:"id" gorm:"primaryKey"`
+	Lat		float64		`json:"lat"`
+	Lng		float64		`json:"lng"`
+	Neighbours	[]*Node		`json:":neighbours" gorm:"many2many:node_neighbours;association_jointable_foreignkey:neighbour_id"`
 }
 
-type Edge struct {
-	EID     	int		`json:"id" gorm:"primaryKey"`
-	Node1ID		int     	`json:"node1_id"`
-	Node2ID		int		`json:"node2_id"`
-	Cost    	int     	`json:"cost"`
-}
 
-func contains(list []*Edge, x int) bool {
-	for _, item := range list {
-		if item.EID == x {
-			return true
-		}
-	}
-	return false
-}
-
-func ConnectDatabase() {
+func ConnectDatabase2() {
     // Replace the connection details below with your own MySQL database configuration
 	dsn := "arturcs:123123123@tcp(127.0.0.1:3306)/gat_db?charset=utf8mb4&parseTime=True&loc=Local"
     database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -41,7 +27,7 @@ func ConnectDatabase() {
         panic("Failed to connect to database!")
     }
 
-    err = database.AutoMigrate(&Node{}, &Edge{})
+    err = database.AutoMigrate(&Node{})
     if err != nil {
         fmt.Println("Failed to auto migrate database schema")
         return
@@ -51,18 +37,24 @@ func ConnectDatabase() {
     db = database
 }
 
-func FindNode(id int) (*Node, error){
-	var node Node
-	res := db.First(&node,"id = ?", id)
-	if res.RowsAffected == 0 || res.Error != nil {
-		fmt.Println("Node is nil")
-		fmt.Println(node)
-		return nil, errors.New("Not in database")
-	} 
+func AddShit(n int) (*Node, error){
 
-	return &node, nil
+	node, err := FindNode(n)
 
+	if err != nil {
+		return nil, errors.New("Error: Noden not in database")
+	}
+	node.Neighbours = append(node.Neighbours, &Node{
+		Lat: 69.420,
+		Lng: 420.69,
+	})
+	res := db.Model(node).Updates(&node)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Error: failed to add shit to node")
+	}
+	return node, nil
 }
+
 
 func AddNode(n *Node) (*Node, error){
 	fmt.Println("Adding node " ,n)
@@ -72,67 +64,58 @@ func AddNode(n *Node) (*Node, error){
 	if res.RowsAffected == 0{
 		return nil, errors.New("Couldn't add node to database")
 	}
-
+	
 	return n, nil
 }
-
-	// eid  := strconv.Itoa(n1.ID) +  "." + strconv.Itoa(n2.ID) 		// form the Edge ID in a cool way
-	// rev_eid := strconv.Itoa(n2.ID) + "." + strconv.Itoa(n1.ID) 		// reverse so no double edging
-
-	// var ed Edge
-
-	// r := db.First(&ed, "e_id = ?", eid)			// The database is queryed twice so that if A->B exists You cant add
-	// r2 := db.First(&ed, "e_id = ?", rev_eid)		
-
-
-func AddEdge(n1, n2 *Node, cost int) (*Edge, error){ 
-
-	fmt.Println("one-", n1, "two-",n2)
-
-	//	The function assumes both n1, and n2 exist in the database
-	//	The check to see if the exist or not and handling of the unhappy path are done sin the API route
-		
-	e := &Edge{
-		Node1ID:	n1.ID,
-		Node2ID: 	n2.ID,
-		Cost: 		cost,
-	}
-
-	if contains(n1.Edges, e.EID) || contains(n2.Edges, e.EID){
-		return e, errors.New("Edge already exists")	
-	}
-
-	res := db.Create(e)
-
-	if res.RowsAffected == 0{
-		return nil, errors.New("Failed to create Edge")
-	}
-
-	n1.Edges = append(n1.Edges, e)
-	n2.Edges = append(n2.Edges, e)
-	db.Save(n1)
-	db.Save(n2)
-	return e, nil
+func FindNode(id int) (*Node, error){
+	fmt.Println("FindNode ran")
+	var node Node
+	res := db.First(&node,"id = ?", id)
+	if res.RowsAffected == 0 || res.Error != nil {
+		fmt.Println("Node is nil")
+		fmt.Println(node)
+		return nil, errors.New("Not in database")
+	} 
+	return &node, nil
 }
 
-// 																			BIG THOUGHT:
-// 			When traversing a graph like this, Start or End are not strongly associated, which means they can be traversed in any order, 
-// 			That means that in a Edge with ID 4-19, you would expect to traverse 4->19, and then have to query for a 19-4 edge to traverse the reverse
-// 			You can simply check if the Node you are visiting (In this case lets say 19) is the Node1 or Node2 in the Edge and then visit the Node that is not
-
-// TODO IMPLEMENT TRAVERSE
 
 
-func AllEdges() ([]*Edge, error){
-	var edges []*Edge
+func AddEdge(n1, n2 *Node) ([]*Node ,error){ 	
+	fmt.Println("AddEdge ran")
 
-	res := db.Find(&edges)
+	node1, err1 := FindNode(n1.ID)
+	node2, err2 := FindNode(n2.ID)
 
-	if res.Error != nil{
-		return nil, errors.New("Failed to find all Edges")
+	if err1 != nil {
+		node1, err1  = AddNode(n1)
+		if err1 != nil{
+			return nil, errors.New("Failed to add first node to db")
+		}
 	}
 
-	return edges, nil
+	if err2 != nil {
+		node2, err2  = AddNode(n2)
+		if err2 != nil{
+			return nil, errors.New("Failed to add second node to db")
+		}
+	}
+	return []*Node{node1, node2}, nil
+}
+func ConnectNodes(node1, node2 *Node) error {
+    fmt.Println("Connecting nodes")
+
+    node1.Neighbours = append(node1.Neighbours, node2)
+    if err := db.Model(node1).Association("Neighbours").Append(node2); err != nil {
+        return err
+    }
+
+    node2.Neighbours = append(node2.Neighbours, node1)
+    if err := db.Model(node2).Association("Neighbours").Append(node1); err != nil {
+        return err
+    }
+
+    return nil
 }
 
 func AllNodes() ([]*Node, error){
@@ -143,6 +126,12 @@ func AllNodes() ([]*Node, error){
 	if res.Error != nil{
 		return nil, errors.New("Failed to find all nodes")
 	}
-
 	return nodes, nil
 }
+/*
+//	When traversing a graph like this, Start or End are not strongly associated, which means they can be traversed in any order, 
+// 	That means that in a Edge with ID 4-19, you would expect to traverse 4->19, and then have to query for a 19-4 edge to traverse the reverse
+// 	You can simply check if the Node you are visiting (In this case lets say 19) is the Node1 or Node2 in the Edge and then visit the Node that is not
+
+// TODO IMPLEMENT TRAVERSE
+*/
