15022b2344a075980ccbcc06c5f5a5996e830d70
Artur Cs <arturcamerasegat@gmail.com>
2023-05-05 00:33:23 -0300

    fixed square conatainment bug

--- util/coordinate.go
+++ util/coordinate.go
@@ -41,21 +41,10 @@ func (c Coord) DistanceToInMeters(other Coord) float32 {
 } 
 
 
-func (co * Coord) IsInSquare(sq Square)  bool{
-    valid_lat := false
-    valid_lng := false
+func (co *Coord) IsInSquare(sq Square) bool {
+    validLat := co.Lat <= sq.Top.Lat && co.Lat >= sq.Bot.Lat
+    validLng := co.Lng >= sq.Top.Lng && co.Lng <= sq.Bot.Lng
 
-    if co.Lat >= sq.Bot.Lat && co.Lat <= sq.Top.Lat{
-        valid_lat = true
-    }
-
-    if co.Lng >= sq.Bot.Lng && co.Lng <= sq.Top.Lng{
-        valid_lng = true
-    }
-
-    if valid_lng && valid_lat{
-        return true
-    }
-    
-    return false 
+    return validLat && validLng
 }
+
