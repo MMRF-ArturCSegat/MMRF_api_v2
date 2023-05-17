bc1194caa03b8661365bae4563727ce7775b159c
Artur Cs <arturcamerasegat@gmail.com>
2023-05-16 21:37:30 -0300

    .gitignore and .env should be working properly

--- db/init.go
+++ db/init.go
@@ -2,15 +2,51 @@ package db
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var db *gorm.DB
 
+
+
+func loadEnvVariables(filePath string) (map[string]string, error) {
+	err := godotenv.Load(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+
+    getEnvVariable := func (key string) string {
+        value := os.Getenv(key)
+        return value
+    }
+
+	envVars := make(map[string]string)
+	envVars["DB_HOST"] = getEnvVariable("DB_HOST")
+	envVars["DB_USER_NAME"] = getEnvVariable("DB_USER_NAME")
+	envVars["DB_PSSW"] = getEnvVariable("DB_PSSW")
+	envVars["DB_NAME"] = getEnvVariable("DB_NAME")
+	envVars["DB_PORT"] = getEnvVariable("DB_PORT")
+
+	return envVars, nil
+}
+
+
+
 func ConnectDatabase2() {
-    // Replace the connection details below with your own PostgreSQL database configuration
-    dsn := "host=localhost user=arturcs password=123123123 dbname=gatdb port=5432 sslmode=disable TimeZone=UTC"
+    envVars, err := loadEnvVariables(".env")
+
+    if err != nil{
+        fmt.Println("Failed to load .env vairables")
+        return
+    }
+
+    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
+                    envVars["DB_HOST"],envVars["DB_USER_NAME"],envVars["DB_PSSW"],envVars["DB_NAME"],envVars["DB_PORT"], )
     database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
         panic("Failed to connect to database!")
