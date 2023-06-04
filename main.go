271981ccb1d341a9646550e858494060297e85e1
Artur Cs <arturcamerasegat@gmail.com>
2023-06-04 18:51:52 -0300

    added the async execution of clean up of expired sessions

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"github.com/UFSM-Routelib/routelib_api/routes"
+	"github.com/UFSM-Routelib/routelib_api/sessions"
 )
 
 func main(){
 	r := routes.SetupRouter()
+    go sessions.CleanExpiredSessions()
 	r.Run(":1337")
 }
